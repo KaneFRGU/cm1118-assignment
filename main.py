1c82b90ddb784fa2005057ff468125533de2cc22
KaneFRGU <146110708+KaneFRGU@users.noreply.github.com>
2024-03-18 15:58:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,66 @@
-print("rahhhhhhhhhhhhhh")
+ Importing pygame module
+
+import pygame
+from pygame.locals import *
+ 
+# initiate pygame and give permission
+# to use pygame's functionality.
+pygame.init()
+ 
+# create the display surface object
+# of specific dimension.
+window = pygame.display.set_mode((600, 600))
+
+jumping = False
+
+ygravity = 1
+jumpheight = 20
+yvelocity = jumpheight
+
+ypos = 500
+run = True
+while run:
+    # Fill the scree with white color
+    window.fill((255, 255, 255))
+    
+    
+    # Define the colors we will use in RGB format
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    blue = (0, 0, 255)
+    green = (0, 255, 0)
+    red = (255, 0, 0)
+    pi = 3.141592653
+     # All drawing code happens after the for loop and but
+    # inside the main while done==False loop.
+  
+   
+ 
+    # Draw on the screen a green line from (0,0) to (50.75)
+    # 5 pixels wide.
+    pygame.draw.line(window, green, [0, 500], [600, 500], 5)
+
+    
+    cube = pygame.draw.rect(window, black, [350, ypos, 100, 100])
+
+
+    
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        keyspressed = pygame.key.get_pressed()
+
+        if keyspressed[pygame.K_SPACE]:
+            jumping = True
+
+        if jumping:
+            ypos -= yvelocity
+            yvelocity -= ygravity
+            if yvelocity < -jumpheight:
+                jumping = False
+                yvelocity = jumpheight
+            
+    # Draws the surface object to the screen.
+    pygame.display.flip()
